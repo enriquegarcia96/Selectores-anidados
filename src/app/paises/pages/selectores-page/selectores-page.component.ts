250fbde10a3fb66c1ada9f4cf5cb5b959fab7ca2
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-02-04 17:39:04 -0600

    refactor: 💡 creacion del formBuilder

--- src/app/paises/pages/selectores-page/selectores-page.component.ts
+++ src/app/paises/pages/selectores-page/selectores-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesServiceService } from '../../services/paises-service.service';
 
 @Component({
   selector: 'app-selectores-page',
   templateUrl: './selectores-page.component.html',
-  styleUrls: ['./selectores-page.component.css']
+  styleUrls: ['./selectores-page.component.css'],
 })
 export class SelectoresPageComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+  });
+
+  //llenar selectores
+  regiones: string[] = [];
+
+
+  constructor(
+    private fb: FormBuilder,
+    private paisesService: PaisesServiceService
+  ) {}
 
   ngOnInit(): void {
+
+    this.regiones = this.paisesService.regiones;
+
   }
 
+  guardar() {
+    console.log(this.miFormulario.value);
+  }
 }
+
