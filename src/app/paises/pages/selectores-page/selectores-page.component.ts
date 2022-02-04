f5cd6f12b6e6904fb7020ac9878e2eda520a4c21
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-02-04 12:40:50 -0600

    feat: 🎸 componente de los selectores

---
+++ src/app/paises/pages/selectores-page/selectores-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectores-page',
+  templateUrl: './selectores-page.component.html',
+  styleUrls: ['./selectores-page.component.css']
+})
+export class SelectoresPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
