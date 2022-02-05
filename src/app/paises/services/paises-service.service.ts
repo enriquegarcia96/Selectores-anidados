2071a2428b5123899df6a9082998139df9d8da06
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-02-04 22:02:16 -0600

    refactor: 💡 retornando un obervable de tipo null

--- src/app/paises/services/paises-service.service.ts
+++ src/app/paises/services/paises-service.service.ts
@@ -1,14 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { PaisSmall } from '../interfaces/paises.interface';
+import { Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisesServiceService {
-
-  private _baseUrl: string = 'https://restcountries.com/v3.1'
+  private _baseUrl: string = 'https://restcountries.com/v3.1';
 
   private _regiones: Array<string> = [
     'Africa',
@@ -25,10 +24,17 @@ export class PaisesServiceService {
   constructor(private http: HttpClient) {}
 
   //lea la region y traer los datos
-  getPaisesPorRegion(region: string): Observable<PaisSmall[]>{
-    const url : string = `${this._baseUrl}/region/${region}?fields=cioc,name`
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]> {
+    const url: string = `${this._baseUrl}/region/${region}?fields=cioc,name`;
     return this.http.get<PaisSmall[]>(url);
   }
 
+  getPaisPorCodigo(codigo: string): Observable<Pais | null> {
+    if (!codigo) {
+      return of(null);
+    }
 
+    const url = `${this._baseUrl}/alpha/${codigo}`;
+    return this.http.get<Pais>(url);
+  }
 }
