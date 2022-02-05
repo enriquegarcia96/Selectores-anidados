4a48f23a33181c42ddc732ef3a75f29761f1829a
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-02-04 18:50:48 -0600

    refactor: 💡 obteniendo los paises

--- src/app/paises/services/paises-service.service.ts
+++ src/app/paises/services/paises-service.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisesServiceService {
+
+  private _baseUrl: string = 'https://restcountries.com/v3.1'
 
   private _regiones: Array<string> = [
     'Africa',
@@ -17,5 +22,13 @@ export class PaisesServiceService {
     return [...this._regiones];
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  //lea la region y traer los datos
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]>{
+    const url : string = `${this._baseUrl}/region/${region}?fields=cioc,name`
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+
 }
