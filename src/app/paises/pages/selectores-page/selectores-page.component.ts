8a8cab62e80a3ddce4f3c2461d4450cdad601458
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-02-04 18:49:55 -0600

    refactor: 💡 llamando al servicio para llenar los selectores

--- src/app/paises/pages/selectores-page/selectores-page.component.ts
+++ src/app/paises/pages/selectores-page/selectores-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesServiceService } from '../../services/paises-service.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selectores-page',
@@ -8,14 +9,14 @@ import { PaisesServiceService } from '../../services/paises-service.service';
   styleUrls: ['./selectores-page.component.css'],
 })
 export class SelectoresPageComponent implements OnInit {
-
   miFormulario: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
+    pais: ['', Validators.required]
   });
 
   //llenar selectores
   regiones: string[] = [];
-
+  paises: PaisSmall[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -23,13 +24,20 @@ export class SelectoresPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
     this.regiones = this.paisesService.regiones;
 
+    // cuando cambie la region en el selector
+    this.miFormulario.get('region')?.valueChanges.subscribe((region) => {
+      console.log(region);
+
+      this.paisesService.getPaisesPorRegion(region).subscribe((paises) => {
+        console.log(paises);
+        this.paises = paises;
+      });
+    });
   }
 
   guardar() {
     console.log(this.miFormulario.value);
   }
 }
-
